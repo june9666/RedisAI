5bd9731d08c5ff3aee1465a33ebc6b0db73e5dc8
hhsecond <sherin@tensorwerk.com>
2019-10-03 22:13:17 +0530

    clean up

--- test/basic_tests.py
+++ test/basic_tests.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../opt/readies"))
-
+import paella
 
 '''
 python -m RLTest --test basic_tests.py --module path/to/redisai.so
